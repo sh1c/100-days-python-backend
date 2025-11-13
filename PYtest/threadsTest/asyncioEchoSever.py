9f9e0a6869a9623f8390046b983b9cf8ee9c7e3b
SH1C <2106835724@qq.com>
2025-11-13 22:42:49 +0800

    day18

--- PYtest/threadsTest/asyncioEchoSever.py
+++ PYtest/threadsTest/asyncioEchoSever.py
@@ -1,31 +1,44 @@
 #!/usr/bin/env python3
+
+
+# 协程函数中获得实例前需要加await关键字,不然返回的是携程对象
+
 import asyncio
 
 HOST, PORT = "127.0.0.1", 7777
 BUFSIZE = 1024
 
 
-async def handle_echo(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-    addr = writer.get_extra_info("peername")
-    print(f"[+] {addr} connected")
+async def echo(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    add = writer.get_extra_info("peername")
+    print(f"{add}正在连接")
     try:
-        while data := await reader.read(BUFSIZE):
+        while True:
+            data = await reader.read(BUFSIZE)
+            if data == None:
+                break
             writer.write(data)
             await writer.drain()
+
     except (ConnectionResetError, BrokenPipeError):
         pass
+
     finally:
-        print(f"[-] {addr} disconnected")
+        print(f"{add}已断开连接")
         writer.close()
         await writer.wait_closed()
 
 
 async def main():
-    server = await asyncio.start_server(handle_echo, HOST, PORT)
-    print(f"[*] Serving on {server.sockets[0].getsockname()}")
-    async with server:  # 退出时自动 close/wait_closed
+    server = await asyncio.start_server(echo, HOST, PORT)
+    print(f"服务器为{server.sockets[0].getsockname()}")
+    async with server:
         await server.serve_forever()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())  # 一行启动事件循环
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        # 不加任何打印，直接正常结束
+        pass
